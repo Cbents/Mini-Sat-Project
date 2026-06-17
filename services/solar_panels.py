875e2fbeb9cf7c7e7af7519c51eed05369dde9ae
Baani Sandhu <165845602+B0nBons@users.noreply.github.com>
2026-06-17 12:30:31 -0600

    Add servo 3

--- services/solar_panels.py
+++ services/solar_panels.py
@@ -2,16 +2,21 @@ from gpiozero import AngularServo
 from time import sleep
 
 # Initialize the servo on GPIO 18 with 180-degree limits
-servo1 = AngularServo(18, min_angle=0, max_angle=180)
+servo1 = AngularServo(12, min_angle=0, max_angle=180)
 
 servo1.angle = 90
 
 # Servo TWO!!!!
-servo2 = AngularServo(19, min_angle=0, max_angle=180)
+servo2 = AngularServo(32, min_angle=0, max_angle=180)
 
 servo2.angle = 90
+
+servo3 = AngularServo(35, min_angle=0, max_angle=180)
+
+servo3.angle = 45
 
 sleep(2) # Wait 2 seconds so it has time to reach the position
 
 servo1.close() # Safely clean up the pin
 servo2.close() # Safely clean up the pin
+servo3.close()
